58ae99a1bdd10b3c4b3f8ae1ea34df73da935ebd
David Hary <791370+ATECoder@users.noreply.github.com>
2025-11-18 19:19:55 -0800

    - Remove legacy solution files.

--- src/units.amounts/standard.units/StandardUnits/LuminousIntensityUnits.cs
+++ src/units.amounts/standard.units/StandardUnits/LuminousIntensityUnits.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// https://github.com/dotnet/runtime/blob/f21a2666c577306e437f80fe934d76cdb15072a5/src/libraries/Common/src/Interop/Windows/Shell32/Interop.SHGetKnownFolderPath.cs
+
 namespace cc.isr.UnitsAmounts.StandardUnits;
 
 /// <summary>   The luminous intensity units. </summary>
